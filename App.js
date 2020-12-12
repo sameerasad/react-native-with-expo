faacbe04a885f5a43c46bbf5e3c4ca7f67bcb834
sameerasad <ssameerkhan525@gmail.com>
2020-12-12 21:46:28 +0500

    border-radiu-borderWidth And elevation

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, ViewBase } from "react-native";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen.js";
 import WelcomeScreen from "./app/screens/WelcomeScreen.js";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <View style={styles.container} />;
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 100,
+    top: "40%",
+    borderRadius: 35,
+    borderWidth: 10,
+    elevation: 50,
+
+    flexDirection: "column",
+  },
+});
