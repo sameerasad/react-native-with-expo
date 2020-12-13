bda90458ea210a2a7c853ea1dc40406bf9fdd810
sameerasad <ssameerkhan525@gmail.com>
2020-12-13 21:25:54 +0500

    commit export modules

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View, Image, ViewBase } from "react-native";
 
-import ViewImageScreen from "./app/screens/ViewImageScreen.js";
-import WelcomeScreen from "./app/screens/WelcomeScreen.js";
+//import ViewImageScreen from "./app/screens/ViewImageScreen.js";
+//import WelcomeScreen from "./app/screens/WelcomeScreen.js";
 
 export default function App() {
   return <WelcomeScreen />;
