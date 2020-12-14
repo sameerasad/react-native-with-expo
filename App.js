3c071bf662fa00c24e23ae292dcc65a4737633ca
sameerasad <ssameerkhan525@gmail.com>
2020-12-14 18:23:56 +0500

    padding and margin propperty

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View, Image, ViewBase } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 
-//import ViewImageScreen from "./app/screens/ViewImageScreen.js";
-//import WelcomeScreen from "./app/screens/WelcomeScreen.js";
+import ViewImageScreen from "./app/screens/ViewImageScreen.js";
+import WelcomeScreen from "./app/screens/WelcomeScreen.js";
+import colors from "./app/config/colors.js";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View style={styles.container}>
+      <Text>HELLOworld</Text>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
@@ -15,9 +20,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width: "100%",
     height: 100,
-    top: "40%",
-    borderRadius: 35,
-    borderWidth: 10,
+    paddingVertical: "100%",
+
+    borderRadius: 0,
+    borderWidth: 0,
     elevation: 60,
 
     flexDirection: "column",
